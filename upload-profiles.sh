6df57f5d143687f1e9e00b87066dc6c39641a0b9
Lol3rrr <lol3r@lol3r.net>
2024-04-24 18:41:32 +0200

    Profiling should now upload profiles to pyroscope

---
+++ upload-profiles.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e;
+
+echo "Uploading Profiles"
+
+BRANCH=$(git symbolic-ref --short HEAD)
+COMMIT=$(git rev-parse HEAD)
+
+root_benchmarks=target/criterion
+for bench_harness in "$root_benchmarks"/*
+do
+  harnessname=$(basename "$bench_harness")
+  echo "  Harness: $harnessname"
+  for benchmark in "$bench_harness"/*
+  do
+    benchname=$(basename "$benchmark")
+    echo "    Benchmark: $benchname"
+
+    for param in "$benchmark"/*
+    do
+      paramname=$(basename "$param")
+      echo "      Param: $paramname"
+
+      profilepath="$param"/profile.pb
+      profilecli upload \
+        --extra-labels=service_name=s3db \
+        --extra-labels=harness="$harnessname" \
+        --extra-labels=benchmark="$benchname" \
+        --extra-labels=param="$paramname" \
+        --extra-labels=branch="$BRANCH" \
+        --extra-labels=commit="$COMMIT" \
+        "$profilepath"
+    done
+  done
+done
