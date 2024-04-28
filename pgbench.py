2579ba723a8b6d18c5bafa703d6a1001f12ad2a9
Lol3rrr <lol3r@lol3r.net>
2024-04-28 20:40:52 +0200

    Add flushing to output

--- pgbench.py
+++ pgbench.py
@@ -3,19 +3,19 @@ import time
 import signal
 import sys
 
-print(f"Compiling S3DB in release mode")
+print(f"Compiling S3DB in release mode", flush=True)
 build_res = subprocess.run(["cargo", "build", "--release"], capture_output=True)
 if build_res.returncode != 0:
     sys.exit(-1)
 
-print(f"Starting database")
+print(f"Starting database", flush=True)
 s3db_process = subprocess.Popen(["target/release/s3db"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-print(f"Waiting 10 Seconds for database to get up and running")
+print(f"Waiting 10 Seconds for database to get up and running", flush=True)
 time.sleep(10)
 
 if s3db_process.poll() is not None:
-    raise ValueError("S3DB process stopped")
+    raise ValueError("S3DB process stopped", flush=True)
 
 bench_duration = 30
 subprocess.run(["pgbench", "-h", "localhost", "-i"])
