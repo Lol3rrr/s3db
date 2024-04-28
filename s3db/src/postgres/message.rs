91f55e214d775f2b4c047720f1e90ee82a6942ed
Lol3rrr <lol3r@lol3r.net>
2024-04-28 23:48:26 +0200

    Fix formatting

--- s3db/src/postgres/message.rs
+++ s3db/src/postgres/message.rs
@@ -96,7 +96,7 @@ impl Message {
         match ty {
             b'Q' => {
                 let (rem, query) = parse_string(data)?;
-                Ok((rem, Self::Query { query}))
+                Ok((rem, Self::Query { query }))
             }
             b'D' => {
                 let (rem, (dkind, name)) = nom::sequence::tuple((
@@ -109,11 +109,15 @@ impl Message {
                 Ok((rem, Self::Describe { kind: dkind, name }))
             }
             b'E' => {
-                let (rem, (portal, rows)) = nom::sequence::tuple((
-                    parse_string,
-                    nom::number::streaming::be_i32,
-                ))(data)?;
-                Ok((rem, Self::Execute { portal, max_rows: rows }))
+                let (rem, (portal, rows)) =
+                    nom::sequence::tuple((parse_string, nom::number::streaming::be_i32))(data)?;
+                Ok((
+                    rem,
+                    Self::Execute {
+                        portal,
+                        max_rows: rows,
+                    },
+                ))
             }
             b'P' => {
                 let (rem, (dest, query, type_count)) = nom::sequence::tuple((
@@ -121,52 +125,56 @@ impl Message {
                     parse_string,
                     nom::number::streaming::be_i16,
                 ))(data)?;
-                Ok((rem, Self::Parse {
-                    destination: dest,
-                    query,
-                    data_types: Vec::new(),
-                }))
+                Ok((
+                    rem,
+                    Self::Parse {
+                        destination: dest,
+                        query,
+                        data_types: Vec::new(),
+                    },
+                ))
             }
             b'B' => {
-                let (rem, (destination, statement, mut pformats, pvalues, result_formats)) = nom::sequence::tuple((
-                    parse_string,
-                    parse_string,
-                    Self::parse_bind_parameters,
-                    Self::parse_bind_values,
-                    Self::parse_bind_format,
-                ))(data)?;
+                let (rem, (destination, statement, mut pformats, pvalues, result_formats)) =
+                    nom::sequence::tuple((
+                        parse_string,
+                        parse_string,
+                        Self::parse_bind_parameters,
+                        Self::parse_bind_values,
+                        Self::parse_bind_format,
+                    ))(data)?;
 
                 if pformats.is_empty() {
-                        pformats.extend((0..pvalues.len()).map(|_| FormatCode::Text));
-                    } else if pformats.len() == 1 {
-                        let first_format = pformats.first().cloned().unwrap();
-                        pformats.extend((1..pvalues.len()).map(|_| first_format.clone()));
-                    }
+                    pformats.extend((0..pvalues.len()).map(|_| FormatCode::Text));
+                } else if pformats.len() == 1 {
+                    let first_format = pformats.first().cloned().unwrap();
+                    pformats.extend((1..pvalues.len()).map(|_| first_format.clone()));
+                }
 
-                Ok((rem, Self::Bind {
+                Ok((
+                    rem,
+                    Self::Bind {
                         destination,
                         statement,
                         parameter_formats: Vec::new(),
                         parameter_values: pvalues,
                         result_column_format_codes: result_formats,
-                    }))
+                    },
+                ))
             }
             b'd' => {
                 let (rem, data) = Self::parse_copy_data(data)?;
                 Ok((rem, Self::CopyData { data }))
             }
-            b'c' => {
-                Ok((data, Self::CopyDone))
-            }
-            b'S' => {
-                Ok((data, Self::Sync_))
-            }
-            b'X' => {
-                Ok((data, Self::Terminate))
-            }
+            b'c' => Ok((data, Self::CopyDone)),
+            b'S' => Ok((data, Self::Sync_)),
+            b'X' => Ok((data, Self::Terminate)),
             other => {
                 tracing::error!("Unknown Raw-Type: {:?}", other);
-                Err(nom::Err::Failure(nom::error::Error::new(data, nom::error::ErrorKind::Tag)))
+                Err(nom::Err::Failure(nom::error::Error::new(
+                    data,
+                    nom::error::ErrorKind::Tag,
+                )))
             }
         }
     }
@@ -233,9 +241,11 @@ mod tests {
 
     #[test]
     fn parse_query() {
-        let (remaining, result) =
-            Message::parse_internal(b'Q', &[b'S', b'E', b'L', b'E', b'C', b'T', b' ', b'1', b'\0'])
-                .unwrap();
+        let (remaining, result) = Message::parse_internal(
+            b'Q',
+            &[b'S', b'E', b'L', b'E', b'C', b'T', b' ', b'1', b'\0'],
+        )
+        .unwrap();
         assert_eq!(&[] as &[u8], remaining);
         assert_eq!(
             Message::Query {
@@ -300,10 +310,13 @@ mod tests {
 
     #[test]
     fn parse_parse() {
-        let (remaining, result) = Message::parse_internal(b'P', &[
-            b'd', b'e', b's', b't', b'\0', b'S', b'E', b'L', b'E', b'C', b'T', b' ', b'1',
-            b'\0', 0, 0,
-        ])
+        let (remaining, result) = Message::parse_internal(
+            b'P',
+            &[
+                b'd', b'e', b's', b't', b'\0', b'S', b'E', b'L', b'E', b'C', b'T', b' ', b'1',
+                b'\0', 0, 0,
+            ],
+        )
         .unwrap();
         assert_eq!(&[] as &[u8], remaining);
         assert_eq!(
@@ -318,13 +331,16 @@ mod tests {
 
     #[test]
     fn parse_bind_no_parameters() {
-        let (remaining, result) = Message::parse_internal(b'B', &[
-            b'd', b'e', b's', b't', b'\0', // destination
-            b'n', b'a', b'm', b'e', b'\0', // name
-            0, 0, // parameter format count
-            0, 0, // parameter value count
-            0, 0, // result column count
-        ])
+        let (remaining, result) = Message::parse_internal(
+            b'B',
+            &[
+                b'd', b'e', b's', b't', b'\0', // destination
+                b'n', b'a', b'm', b'e', b'\0', // name
+                0, 0, // parameter format count
+                0, 0, // parameter value count
+                0, 0, // result column count
+            ],
+        )
         .unwrap();
         assert_eq!(&[] as &[u8], remaining);
         assert_eq!(
@@ -341,15 +357,18 @@ mod tests {
 
     #[test]
     fn parse_bind_values() {
-        let (remaining, result) = Message::parse_internal(b'B', &[
-            b'd', b'e', b's', b't', b'\0', // destination
-            b'n', b'a', b'm', b'e', b'\0', // name
-            0, 0, // parameter format count
-            0, 2, // parameter value count
-            0, 0, 0, 4, 1, 2, 3, 4, // First Parametere value
-            0, 0, 0, 5, 5, 4, 3, 2, 1, // Second Parametere value
-            0, 0, // result column count
-        ])
+        let (remaining, result) = Message::parse_internal(
+            b'B',
+            &[
+                b'd', b'e', b's', b't', b'\0', // destination
+                b'n', b'a', b'm', b'e', b'\0', // name
+                0, 0, // parameter format count
+                0, 2, // parameter value count
+                0, 0, 0, 4, 1, 2, 3, 4, // First Parametere value
+                0, 0, 0, 5, 5, 4, 3, 2, 1, // Second Parametere value
+                0, 0, // result column count
+            ],
+        )
         .unwrap();
         assert_eq!(&[] as &[u8], remaining);
         assert_eq!(
